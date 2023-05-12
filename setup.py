cf81d3097f919bf94487a10499a7b6e278636ac5
William Hart <whart222@gmail.com>
2023-05-12 08:36:45 -0400

    Reformatting with black

--- setup.py
+++ setup.py
@@ -15,50 +15,46 @@ def _find_packages(path):
     pkg_list = []
     if not os.path.exists(path):
         return []
-    if not os.path.exists(path+os.sep+"__init__.py"):
+    if not os.path.exists(path + os.sep + "__init__.py"):
         return []
     else:
         pkg_list.append(path)
     for root, dirs, files in os.walk(path, topdown=True):
         if root in pkg_list and "__init__.py" in files:
             for name in dirs:
-                if os.path.exists(root+os.sep+name+os.sep+"__init__.py"):
-                    pkg_list.append(root+os.sep+name)
-    return [pkg for pkg in map(lambda x:x.replace(os.sep, "."), pkg_list)]
+                if os.path.exists(root + os.sep + name + os.sep + "__init__.py"):
+                    pkg_list.append(root + os.sep + name)
+    return [pkg for pkg in map(lambda x: x.replace(os.sep, "."), pkg_list)]
 
 
-requires = [
-    'Pyomo',
-    'munch',
-    'hmmlearn',
-    'numpy'
-    ]
-packages = _find_packages('cihmm')
+requires = ["Pyomo", "munch", "hmmlearn", "numpy"]
+packages = _find_packages("cihmm")
 
-setup(name='cihmm',
-      version='1.0',
-      url='https://cee-gitlab.sandia.gov/or-fusion/cihmm',
-      platforms=["any"],
-      description='A python library for constrained inference with Hidden Markov Models',
-      classifiers=[
+setup(
+    name="cihmm",
+    version="1.0",
+    url="https://cee-gitlab.sandia.gov/or-fusion/cihmm",
+    platforms=["any"],
+    description="A python library for constrained inference with Hidden Markov Models",
+    classifiers=[
         #'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Science/Research',
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
         #'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
+        "Natural Language :: English",
         #'Operating System :: MacOS',
         #'Operating System :: Microsoft :: Windows',
-        'Operating System :: Unix',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Software Development :: Libraries :: Python Modules' ],
-      packages=packages,
-      keywords=['hidden markov models'],
-      install_requires=requires
-      #python_requires='>=3.7',
-      )
-
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=packages,
+    keywords=["hidden markov models"],
+    install_requires=requires
+    # python_requires='>=3.7',
+)
